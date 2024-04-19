ea99ceca266eb182efa4e229e6b4717bda7d06de
unknown <brandenek03912@hotmail.com>
2024-04-19 10:31:13 -0400

    core: add content folder

---
+++ content/AShortHike.ModTemplate/Main.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+
+namespace AShortHike.ModTemplate;
+
+[BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
+[BepInDependency("AShortHike.ModdingAPI", "0.1.0")]
+public class Main : BaseUnityPlugin
+{
+    public static ModTemplate ModTemplate { get; private set; }
+
+    private void Awake()
+    {
+        ModTemplate = new ModTemplate();
+    }
+}
